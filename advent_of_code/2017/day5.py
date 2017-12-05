e5de16f990b0cb6884aa3f8b0c6aec5b1c829e1d
Samreen Zarroug <szarroug3@gmail.com>
2017-12-05 10:58:40 -0600

    adding day 5 AoC

---
+++ advent_of_code/2017/day5.py
@@ -0,0 +1,24 @@
+# !/bin/python3
+# http://www.adventofcode.com/2017/day/5
+
+from utils import read_input
+
+
+def get_num_jumps(jump_list):
+    i = 0
+    num_jumps = 0
+
+    while i in jump_list.keys():
+        jump = jump_list[i]
+        if jump > 2:
+            jump_list[i] -= 1
+        else:
+            jump_list[i] += 1
+        i += jump
+        num_jumps += 1
+
+    return num_jumps
+
+
+JUMP_LIST = {i: value for i, value in enumerate(read_input(val_type=int))}
+print('part a:', get_num_jumps(JUMP_LIST))
