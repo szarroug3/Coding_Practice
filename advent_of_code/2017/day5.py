372134704b098d2b50dc474bab3e9618bae17363
Samreen Zarroug <szarroug3@gmail.com>
2017-12-05 17:31:42 -0600

    separating day 5 parts 1 and 2 for AoC

--- advent_of_code/2017/day5.py
+++ advent_of_code/2017/day5.py
@@ -2,9 +2,23 @@
 # http://www.adventofcode.com/2017/day/5
 
 from utils import read_input
+from copy import copy
 
 
-def get_num_jumps(jump_list):
+def get_num_jumps_a(jump_list):
+    i = 0
+    num_jumps = 0
+
+    while i in jump_list.keys():
+        jump = jump_list[i]
+        jump_list[i] += 1
+        i += jump
+        num_jumps += 1
+
+    return num_jumps
+
+
+def get_num_jumps_b(jump_list):
     i = 0
     num_jumps = 0
 
@@ -21,4 +35,5 @@ def get_num_jumps(jump_list):
 
 
 JUMP_LIST = {i: value for i, value in enumerate(read_input(val_type=int))}
-print('part a:', get_num_jumps(JUMP_LIST))
+print('part a:', get_num_jumps_a(copy(JUMP_LIST)))
+print('part b:', get_num_jumps_b(JUMP_LIST))
