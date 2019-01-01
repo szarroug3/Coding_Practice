83ec56afa56abe4d240d455bc95efe5d466f7e30
Samreen Zarroug <szarroug3@gmail.com>
2019-01-01 11:56:45 -0600

    updating day11 and day12

--- advent_of_code/2018/day11.py
+++ advent_of_code/2018/day11.py
@@ -15,12 +15,16 @@ def get_power(x, y):
     return (power // 100 % 10) - 5
 
 
-def get_biggest(grid, size):
-    for width in range(3, 300):
+def get_biggest(grid, sizes):
+    biggest = grid[0][0]
+    for width in sizes:
         block = sum(grid[x:x-width+1 or None, y:y-width+1 or None] for x in range(width) for y in range(width))
         maximum = int(block.max())
-        loc = numpy.where(block == maximum)
-    return loc[0][0] + 1, loc[1][0] + 1, maximum
+        if maximum > biggest:
+            biggest = maximum
+            loc = numpy.where(block == maximum)
+            size = width
+    return loc[0][0] + 1, loc[1][0] + 1, size
 
 
 def get_biggest_size(grid):
@@ -30,12 +34,12 @@ def get_biggest_size(grid):
         x, y, m = get_biggest(grid, i)
         if m > maximum:
             maximum = m
-            data = (x, y, size)
+            data = (x, y, i)
     return data
 
 
 if __name__ == '__main__':
     serial += read_input(val_type=int)
     grid = numpy.fromfunction(get_power, (300, 300))
-    print('part a:', get_biggest(grid, 3)[:2])
-    print('part b:', get_biggest_size(grid))
+    print('part a:', get_biggest(grid, [3])[:2])
+    print('part b:', get_biggest(grid, range(1, 301)))
--- advent_of_code/2018/day12.py
+++ advent_of_code/2018/day12.py
@@ -5,55 +5,82 @@
 from utils import read_input
 
 
+def get_bare_plants(plants):
+    offset = plants.find('#')
+    return plants[offset:plants.rfind('#') + 1], offset
+
+
 def parse(data):
-    plants = {}
-    for i, val in enumerate(data[0].split()[-1]):
-        plants[int(i)] = val
+    plants, offset = get_bare_plants(data[0].split()[-1])
 
     notes = {}
     for note in data[2:]:
         d = note.split()
-        notes[d[0]] = True if d[-1] == '#' else False
+        if d[-1] == '#':
+            notes[d[0]] = True
 
-    return plants, notes
-
-
-def print_plants(plants, gen):
-    print('Gen', gen, 'Min', min(plants.keys()), 'Max', max(plants.keys()))
-    nums = ''
-    p = ''
-    for k, v in plants.items():
-        nums += str(abs(k))[-1]
-        p += v
-
-    print(nums)
-    print(p)
+    return plants, offset, notes
 
 
-def age(plants, notes, gens=20):
-    for i in range(gens):
-        new_plants = {}
-        for j in range(min(plants.keys())-2, max(plants.keys()) + 1):
-            combo = ''
-            for k in range(j-2, j+3):
-                if k not in plants:
-                    plants[k] = '.'
-                combo += plants[k]
+def age_plants(plants, notes):
+    plants = '....' + plants + '....'
+    new_plants = ''
+    for i in range(2, len(plants) - 2):
+        if plants[i-2:i+3] in notes:
+            new_plants += '#'
+        else:
+            new_plants += '.'
+    plants, offset = get_bare_plants(new_plants)
+    return plants, offset - 2
 
-            if notes.get(combo, False):
-                new_plants[j] = '#'
-            else:
-                new_plants[j] = '.'
+
+def get_offset(first, last, rounds, results):
+    offsets = [val['offset'] for val in results.values() if first <= val['index'] <= last]
+    return sum(offsets) * rounds
+
+
+def get_repeat_count(curr_gen, gens, curr_plants, curr_offset, results):
+    gens_left = gens - curr_gen
+
+    first_plants = results[curr_plants]
+    last_plants = max(results.values(), key=lambda p: p['index'])
+
+    if first_plants == last_plants:
+        rounds = gens_left
+        remainder = 0
+    else:
+        round_len = last_plants['index'] - first_plants['index']
+        rounds = gens_left // round_len
+        remainder = gens_left % round_len
+    result_index = first_plants['index'] + remainder
+
+    for val in results.values():
+        if val['index'] == result_index:
+            result_plants = val
+            break
+
+    previous_offset = min([val for val in results.values() if val['plants'] == curr_plants], key=lambda p: p['index'])['offset']
+    rounds_offset = last_plants['offset'] - previous_offset
+    remainder_offset = result_plants['offset'] - previous_offset
+    curr_offset += (rounds_offset * rounds) + (remainder_offset * remainder)
+
+    return sum(i for i, val in enumerate(result_plants['plants'], start=curr_offset) if val == '#')
+
+
+def age_gens(plants, offset, notes, gens=20):
+    results = {}
+    for i, _ in enumerate(range(gens), start=1):
+        if plants in results:
+            return get_repeat_count(i-1, gens, results[plants]['plants'], offset, results)
+        new_plants, plant_offset = age_plants(plants, notes)
+        offset += plant_offset
+        results[plants] = {'index': i, 'plants': new_plants, 'offset': offset}
         plants = new_plants
-        print_plants(plants, i+1)
-    return plants
+    return sum([i + offset for i, val in enumerate(plants) if val == '#'])
 
-
-def count_plants(plants):
-    return sum([k for k, v in plants.items() if v == '#'])
 
 
 if __name__ == '__main__':
-    plants, notes = parse(read_input())
-    print_plants(plants, 0)
-    print('part a:', count_plants(age(plants, notes)))
+    plants, offset, notes = parse(read_input())
+    print('part a:', age_gens(plants, offset, notes))
+    print('part b:', age_gens(plants, offset, notes, gens=50000000000))
