8a8ecb303cdb8535f1704eb24cddad7c964807fd
Samreen Zarroug <samreen.zarroug@hpe.com>
2021-12-27 21:25:58 +0000

    Add days 14-25 folders (not solved) along with their samples and inputs

---
+++ advent_of_code/2021/day14/main.go
@@ -0,0 +1,42 @@
+// https://adventofcode.com/2020/day/#
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"utils/utils"
+)
+
+func PartA(data string, result chan interface{}) {
+	result <- "UPDATE THIS"
+}
+
+func PartB(data string, result chan interface{}) {
+	result <- "UPDATE THIS"
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Input filename required.")
+		return
+	}
+
+	data, err := utils.ReadFileAsString(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// TODO: REMOVE THIS
+	fmt.Println(data)
+
+	a := make(chan interface{})
+	b := make(chan interface{})
+
+	go PartA(data, a)
+	go PartB(data, b)
+
+	fmt.Println("Part A:", <-a)
+	fmt.Println("Part B:", <-b)
+}
