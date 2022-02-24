97a31a250b17f8739c97fac0fd6ab66045bcbc5f
Samreen Zarroug <samreen.zarroug@hpe.com>
2022-02-24 12:37:54 -0600

    Add solution for day23 of AOC 2021

--- advent_of_code/2021/day23/main.go
+++ advent_of_code/2021/day23/main.go
@@ -1,18 +1,70 @@
-// https://adventofcode.com/2020/day/#
+// https://adventofcode.com/2020/day/23
 
 package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"utils/utils"
 )
 
-func PartA(data string, result chan interface{}) {
-	result <- "UPDATE THIS"
+func Parse(data []string) [][]uint8 {
+	buckets := make([][]uint8, 0)
+	for i := 3; i <= 9; i += 2 {
+		buckets = append(buckets, []uint8{data[2][i], data[3][i]})
+	}
+	return buckets
 }
 
-func PartB(data string, result chan interface{}) {
+func PartA(buckets [][]uint8, result chan interface{}) {
+	costs := map[int]int{65: 1, 66: 10, 67: 100, 68: 1000}
+	final := map[int]int{0: 0, 1: 0, 2: 0, 3: 0}
+
+	for i, bucket := range buckets {
+		for j := len(bucket) - 1; j >= 0; j-- {
+			value := int(bucket[j])
+			destination := value - 65
+			if i != destination {
+				break
+			}
+			final[destination]++
+		}
+	}
+
+	fmt.Println(final)
+	energy := 0
+	for i, bucket := range buckets {
+		move := false
+		for j := len(bucket) - 1; j >= 0; j-- {
+			value := int(bucket[j])
+			destination := value - 65
+			depth := 2 - final[destination]
+			if int(value)-65-i != 0 {
+				move = true
+				steps := 0
+				// move out of current room
+				steps += j + 1
+				// move across hallway
+				steps += 2 * int(math.Abs(float64(destination-i)))
+				// move into destination
+				steps += depth
+
+				energy += steps * costs[value]
+				final[destination]++
+				fmt.Println("Moving", value, i, j, steps*costs[value], energy)
+			} else if move {
+				steps := (2 * (j + 1)) + 2
+				energy += steps * costs[value]
+				fmt.Println("Moving", value, i, j, steps*costs[value], energy)
+				final[destination]++
+			}
+		}
+	}
+	result <- energy
+}
+
+func PartB(buckets [][]uint8, result chan interface{}) {
 	result <- "UPDATE THIS"
 }
 
@@ -22,20 +74,20 @@ func main() {
 		return
 	}
 
-	data, err := utils.ReadFileAsString(os.Args[1])
+	data, err := utils.ReadFileAsSlices(os.Args[1], "\n")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	// TODO: REMOVE THIS
-	fmt.Println(data)
+	buckets := Parse(data)
+	fmt.Println(buckets)
 
 	a := make(chan interface{})
 	b := make(chan interface{})
 
-	go PartA(data, a)
-	go PartB(data, b)
+	go PartA(buckets, a)
+	go PartB(buckets, b)
 
 	fmt.Println("Part A:", <-a)
 	fmt.Println("Part B:", <-b)
