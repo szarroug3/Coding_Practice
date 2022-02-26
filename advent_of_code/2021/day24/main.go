b570804a31509cc201a5403eb3827e14ceab3623
Samreen Zarroug <szarroug3@gmail.com>
2022-02-26 02:13:45 -0600

    Add solution for day24 of AOC 2021

--- advent_of_code/2021/day24/main.go
+++ advent_of_code/2021/day24/main.go
@@ -4,39 +4,73 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"utils/utils"
+	"math"
+	"sort"
+	"strconv"
 )
 
-func PartA(data string, result chan interface{}) {
-	result <- "UPDATE THIS"
+func getModel(i int, z int, zPossibilities map[int]map[int][][]int, model string, max bool) string {
+	if i == 14 {
+		return model
+	}
+
+	sort.Slice(zPossibilities[i][z], func(j, k int) bool {
+		if max {
+			return zPossibilities[i][z][j][0] > zPossibilities[i][z][k][0]
+		} else {
+			return zPossibilities[i][z][j][0] < zPossibilities[i][z][k][0]
+		}
+	})
+
+	for _, values := range zPossibilities[i][z] {
+		model += strconv.Itoa(values[0])
+		model = getModel(i+1, values[1], zPossibilities, model, max)
+		if len(model) != 0 {
+			return model
+		}
+	}
+	return ""
 }
 
-func PartB(data string, result chan interface{}) {
-	result <- "UPDATE THIS"
+func PartA() (string, string) {
+	num1 := []int{1, 1, 1, 26, 26, 1, 1, 26, 1, 26, 1, 26, 26, 26}
+	num2 := []int{12, 12, 12, -9, -9, 14, 14, -10, 15, -2, 11, -15, -9, -3}
+	num3 := []int{9, 4, 2, 5, 1, 6, 11, 15, 7, 12, 15, 9, 12, 12}
+
+	zPossibilities := map[int]map[int][][]int{14: {0: {}}}
+
+	for i := 13; i >= 0; i-- {
+		if _, ok := zPossibilities[i]; !ok {
+			zPossibilities[i] = make(map[int][][]int)
+		}
+
+		for w := 9; w >= 1; w-- {
+			for z := range zPossibilities[i+1] {
+				for a := 0; a < num1[i]; a++ {
+					oldz := z*num1[i] + a
+					if oldz%26+num2[i] == w {
+						if oldz/num1[i] == z {
+							zPossibilities[i][oldz] = append(zPossibilities[i][oldz], []int{w, z})
+						}
+					}
+
+					oldz = int(math.Round(float64((z-w-num3[i])/26*num1[i] + a)))
+					if oldz%26+num2[i] != w {
+						if oldz/num1[i]*26+w+num3[i] == z {
+							zPossibilities[i][oldz] = append(zPossibilities[i][oldz], []int{w, z})
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return getModel(0, 0, zPossibilities, "", true), getModel(0, 0, zPossibilities, "", false)
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Input filename required.")
-		return
-	}
+	a, b := PartA()
 
-	data, err := utils.ReadFileAsString(os.Args[1])
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// TODO: REMOVE THIS
-	fmt.Println(data)
-
-	a := make(chan interface{})
-	b := make(chan interface{})
-
-	go PartA(data, a)
-	go PartB(data, b)
-
-	fmt.Println("Part A:", <-a)
-	fmt.Println("Part B:", <-b)
+	fmt.Println("Part A:", a)
+	fmt.Println("Part B:", b)
 }
