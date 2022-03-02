bc676e71b31a2119eff483aa7b69e48e0299eac3
Samreen Zarroug <szarroug3@gmail.com>
2022-03-02 15:57:51 -0600

    Add solution for AOC 2021 day 25

--- advent_of_code/2021/day25/main.go
+++ advent_of_code/2021/day25/main.go
@@ -8,11 +8,64 @@ import (
 	"utils/utils"
 )
 
-func PartA(data string, result chan interface{}) {
-	result <- "UPDATE THIS"
+func PartA(data [][]string, result chan interface{}) {
+	fmt.Println("---")
+	for _, line := range data {
+		fmt.Println(line)
+	}
+	fmt.Println("---")
+	steps := 0
+	for moved := true; moved; steps++ {
+		moved = false
+		curr := data[:]
+		for i := 0; i < len(data); i++ {
+			for j := 0; j < len(data[i]); j++ {
+				if data[i][j] != ">" {
+					continue
+				}
+
+				east := (j + 1) % len(data[i])
+				if data[i][east] == "." {
+					fmt.Println("moving", i, j, "to", i, east)
+					moved = true
+					curr[i][j] = "."
+					curr[i][east] = data[i][j]
+				}
+			}
+		}
+
+		fmt.Println("AFTER EAST:")
+		for _, line := range curr {
+			fmt.Println(line)
+		}
+		fmt.Println("---")
+		for i := 0; i < len(data); i++ {
+			south := (i + 1) % len(data)
+			for j := 0; j < len(data[i]); j++ {
+				if data[i][j] != "v" {
+					continue
+				}
+
+				if data[south][j] == "." {
+					fmt.Println("moving", i, j, "to", south, j)
+					moved = true
+					curr[i][j] = "."
+					curr[south][j] = data[i][j]
+				}
+			}
+		}
+
+		fmt.Println("AFTER SOUTH:")
+		for _, line := range curr {
+			fmt.Println(line)
+		}
+		fmt.Println("---")
+		data = curr
+	}
+	result <- steps
 }
 
-func PartB(data string, result chan interface{}) {
+func PartB(data [][]string, result chan interface{}) {
 	result <- "UPDATE THIS"
 }
 
@@ -22,14 +75,11 @@ func main() {
 		return
 	}
 
-	data, err := utils.ReadFileAsString(os.Args[1])
+	data, err := utils.ReadFileAsStringSlices(os.Args[1], "\n", "")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
-	// TODO: REMOVE THIS
-	fmt.Println(data)
 
 	a := make(chan interface{})
 	b := make(chan interface{})
