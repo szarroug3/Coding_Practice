e0d5eed8977b1db0f1bd1067ced0b598ae68f6d6
Samreen Zarroug <szarroug3@gmail.com>
2022-03-14 12:35:37 -0500

    Add solution for Project Euler problem 18

---
+++ project_euler/problem018.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+// https://projecteuler.net/problem=18
+
+function djikstra(graph) {
+  let visited = [[graph[0][0]]];
+  for (let i = 1; i < graph.length; i++) {
+    visited.push(graph[i].slice());
+    for (let j = 0; j < graph[i].length; j++) {
+      for (let k = j - 1; k < j + 1; k++) {
+        if (k < 0 || k >= visited[i - 1].length) {
+          continue;
+        }
+
+        if (graph[i][j] + visited[i - 1][k] > visited[i][j]) {
+          visited[i][j] = graph[i][j] + visited[i - 1][k];
+        }
+      }
+    }
+  }
+
+  let max = 0;
+  visited[visited.length - 1].forEach((element) => {
+    if (element > max) {
+      max = element;
+    }
+  });
+  return max;
+}
+
+const numbers = [
+  [75],
+  [95, 64],
+  [17, 47, 82],
+  [18, 35, 87, 10],
+  [20, 04, 82, 47, 65],
+  [19, 01, 23, 75, 03, 34],
+  [88, 02, 77, 73, 07, 63, 67],
+  [99, 65, 04, 28, 06, 16, 70, 92],
+  [41, 41, 26, 56, 83, 40, 80, 70, 33],
+  [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+  [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+  [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+  [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+  [63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+  [04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23],
+];
+console.log(djikstra(numbers));
