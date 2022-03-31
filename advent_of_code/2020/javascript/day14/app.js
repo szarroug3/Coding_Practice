7addba4d17d53b03d5f35be14442a7bccb345f7a
Samreen Zarroug <szarroug3@gmail.com>
2022-03-30 21:58:31 -0500

    Finish up javascript solution for AOC 2020 day14

--- advent_of_code/2020/javascript/day14/app.js
+++ advent_of_code/2020/javascript/day14/app.js
@@ -1,4 +1,4 @@
-// https://adventofcode.com/2020/day/#
+// https://adventofcode.com/2020/day/14
 
 const { readFile } = require('../utils/input');
 
@@ -8,13 +8,11 @@ function parse(data) {
   data.forEach((instruction) => {
     if (instruction.includes('mask')) {
       let mask = {};
-      let maskData = data[0].split(' = ')[1];
+      let maskData = instruction.split(' = ')[1];
       for (let i = 0; i < maskData.length; i++) {
-        if (maskData[i] != 'X') {
-          mask[i] = maskData[i];
-        }
+        mask[i] = maskData[i];
       }
-      instructions.push([mask]);
+      instructions.push(mask);
     } else {
       let match = instruction.match(re);
       let address = parseInt(match[1]);
@@ -28,19 +26,17 @@ function parse(data) {
 async function partA(instructions) {
   return new Promise((resolve) => {
     let memory = {};
-    let mask = ''.padStart(36, '0');
+    let mask = {};
     instructions.forEach((instruction) => {
-      if (instruction.length == 1) {
-        mask = instruction[0];
-      } else {
+      if (instruction instanceof Array) {
         let address = instruction[0];
-        let value = maskValue(mask, instruction[1]);
-        memory[address] = value;
+        memory[address] = maskValue(mask, instruction[1]);
+      } else {
+        mask = instruction;
       }
     });
 
     let sum = BigInt(0);
-    console.log(memory);
     for (let value of Object.values(memory)) {
       sum += BigInt(parseInt('0' + value, 2));
     }
@@ -52,15 +48,74 @@ async function partA(instructions) {
 function maskValue(mask, data) {
   data = data.toString(2).padStart(36, '0').split('');
   for (const [i, value] of Object.entries(mask)) {
-    data[i] = value;
+    if (value !== 'X') {
+      data[i] = value;
+    }
   }
   return data.join('');
 }
 
-async function partB(data) {
+async function partB(instructions) {
   return new Promise((resolve) => {
-    resolve('UPDATE THIS');
+    let memory = {};
+    let mask = {};
+    instructions.forEach((instruction) => {
+      if (instruction instanceof Array) {
+        let addresses = maskValueUsingFloating(mask, instruction[0]);
+        addresses.forEach((address) => {
+          memory[address] = instruction[1];
+        });
+      } else {
+        mask = instruction;
+      }
+    });
+
+    let sum = BigInt(0);
+    for (let value of Object.values(memory)) {
+      sum += BigInt(parseInt('0' + value));
+    }
+
+    resolve(sum);
   });
+}
+
+function getCombinations(length) {
+  let values = [];
+  for (let i = 0; i < 2 ** length; i++) {
+    values.push(i.toString(2).padStart(length, '0'));
+  }
+  return values;
+}
+
+function maskValueUsingFloating(mask, data) {
+  data = data.toString(2).padStart(36, '0').split('');
+
+  xCount = 0;
+  Object.values(mask).forEach((element) => {
+    if (element == 'X') {
+      xCount++;
+    }
+  });
+
+  let xCombinations = getCombinations(xCount);
+  let values = [];
+  xCombinations.forEach((combination) => {
+    let combinationIndex = 0;
+    let curr = [];
+    for (const [i, value] of Object.entries(mask)) {
+      if (value === '1') {
+        curr.push(value);
+      } else if (value === 'X') {
+        curr.push(combination[combinationIndex]);
+        combinationIndex++;
+      } else {
+        curr.push(data[i]);
+      }
+    }
+    values.push(curr.join(''));
+  });
+
+  return values;
 }
 
 async function main() {
